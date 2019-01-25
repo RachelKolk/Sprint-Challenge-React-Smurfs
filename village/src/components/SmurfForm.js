82d194caec7304030d5180f109194aacd90ce058
RachelKolk <rach.m.obrien@gmail.com>
2019-01-25 13:49:37 -0500

    router added

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,68 +1,10 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React from 'react';
 
 
-// class SmurfForm extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       smurf: {
-//       id: '',
-//       name: '',
-//       age: '',
-//       height: ''
-//       }
-//     };
-//   }
-
-  // addSmurf = event => {
-  //   event.preventDefault();
-  //   // add code to create the smurf using the api
-
-  //   this.setState({
-  //     name: '',
-  //     age: '',
-  //     height: ''
-  //   });
-  // }
-
-  // handleInputChange = e => {
-  //   this.setState({ [e.target.name]: e.target.value });
-  // };
-
-    // handleInputChanges = e => {
-    //   e.persist();
-    //   this.setState(prevState => {
-    //     return {
-    //       smurf: {
-    //         ...prevState.smurf,
-    //         [e.target.name]: e.target.value
-    //       }
-    //     }
-    //   });
-    // };
-
-    // addSmurf = (e) => {
-    //   e.preventDefault();      
-    //   axios.post("http://localhost:3333/smurfs", this.state.smurf)
-    //     .then(res => {
-    //       console.log('adding smurf');
-    //       // this.setState({smurfs:res.data});
-    //       this.setState({
-    //         name: '',
-    //         age:'',
-    //         height:'',
-    //       });
-    //     })
-    //     .catch(err => console.log(err));
-    // }
 
   function SmurfForm(props) {
 
-  
-
-  
-  
+ 
     return (
       <div className="SmurfForm">
         <form onSubmit={props.addSmurf}>
